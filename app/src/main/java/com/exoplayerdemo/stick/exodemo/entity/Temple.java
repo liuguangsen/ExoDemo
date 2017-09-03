da9f393a86334962e22fe84e505c7dc5925762cb
liuguangsen <1096318053@qq.com>
2017-09-03 16:39:06 +0800

    提交一个实体类

---
+++ app/src/main/java/com/exoplayerdemo/stick/exodemo/entity/Temple.java
@@ -0,0 +1,26 @@
+package com.exoplayerdemo.stick.exodemo.entity;
+
+/**
+ * Created by liugs on 2017/9/3.
+ */
+public class Temple {
+    private String name = "test";
+
+    private char[] nums = {'a', 'b'};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char[] getNums() {
+        return nums;
+    }
+
+    public void setNums(char[] nums) {
+        this.nums = nums;
+    }
+}
