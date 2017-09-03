70163a50d67d07a9c85dec362ca675834566e82a
liuguangsen <1096318053@qq.com>
2017-09-03 16:37:00 +0800

    测试  基本数据类型  和 指针引用类

--- app/src/main/java/com/exoplayerdemo/stick/exodemo/MainActivity.java
+++ app/src/main/java/com/exoplayerdemo/stick/exodemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.exoplayerdemo.stick.exodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.exoplayerdemo.stick.exodemo.entity.Temple;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Temple temple = new Temple();
+        changeStr1(temple);
+        Log.d("liugs", temple.getName() + "  " + temple.getNums()[0]);
+
+        /*int a = 1;
+        String s = "test";
+        char[] nums = {'a', 'b'};
+        char b = 'b';
+        changeStr(a, s, nums, b);
+        Log.d("liugs", a + "  " + s + "  " + nums[0]+"   "+b);*/
+    }
+
+    private void changeStr1(Temple temple) {
+        temple.setName("aaa");
+        temple.setNums(new char[]{'1', '2'});
+    }
+
+    public void changeStr(int a, String s, char[] nums, char b) {
+        a = 2;
+        s = "aaa";
+        //nums[0] = '1';
+        b = 'c';
     }
 }
